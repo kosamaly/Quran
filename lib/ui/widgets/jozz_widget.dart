e67c27f3f64ea896be5be28ca1bfe0fdde82b162
Khaled Osama <khaledosama@Khaleds-MacBook-Air.local>
2023-05-21 21:44:51 +0300

    jozz widget

---
+++ lib/ui/widgets/jozz_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class Jozz extends StatefulWidget {
+  Jozz({Key? key}) : super(key: key);
+  int jozzNum = 0;
+  String jozzName = "";
+  @override
+  State<Jozz> createState() => _JozzState();
+}
+
+class _JozzState extends State<Jozz> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
