c37a7a3f8a83e077dcfb92a151ecd7f9db9ccd84
Khaled Osama <khaledosama@Khaleds-MacBook-Air.local>
2023-05-24 03:46:15 +0300

    last refactor

---
+++ lib/ui/screens/user_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class UserScreen extends StatefulWidget {
+  static String id = "user_screen";
+  UserScreen({Key? key}) : super(key: key);
+  // int jozzNum = 0;
+  // String jozzName = "";
+
+  @override
+  State<UserScreen> createState() => UserScreenState();
+}
+
+class UserScreenState extends State<UserScreen> {
+  List isSelectedList = List.generate(30, (index) => false);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.cyan[300],
+        title: const Text(
+          'الختمه',
+          style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 22),
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 10,
+                    offset: Offset(0, 5), // changes position of shadow
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(16.0),
+                color: Colors.white,
+              ),
+              width: 400,
+              height: 750,
+              child: GridView.count(
+                padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 15,
+                crossAxisCount: 4,
+
+                // Number of columns
+                children: List.generate(30, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isSelectedList[index] = !isSelectedList[
+                            index]; // Convert clicked number to red
+                      });
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage(
+                            'images/4.png',
+                          ),
+                          fit: BoxFit.cover,
+                          repeat: ImageRepeat.repeat,
+                        ),
+                        border: Border.all(
+                            color: isSelectedList[index]
+                                ? Colors.greenAccent
+                                : Colors.white,
+                            width: 5),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(80)),
+                        color:
+                            isSelectedList[index] ? Colors.green : Colors.white,
+                      ),
+                      child: Text(
+                        '${index + 1}',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: isSelectedList[index]
+                              ? Colors.green
+                              : Colors.black54,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/widgets/jozz_widget.dart
+++ lib/ui/widgets/jozz_widget.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/cupertino.dart';
 
-class Jozz extends StatefulWidget {
-  Jozz({Key? key}) : super(key: key);
-  int jozzNum = 0;
-  String jozzName = "";
-  @override
-  State<Jozz> createState() => _JozzState();
-}
-
-class _JozzState extends State<Jozz> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
